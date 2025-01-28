bd86bdf1b3b268a45ad31c26fc2723654662bbf3
tanveerkhan4u <khantanveer6077@gmail.com>
2025-01-28 18:42:12 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,19 +17,22 @@ import Tour4 from './Pages/Tour4'
 import Tour5 from './Pages/Tour5'
 import Tour6 from './Pages/Tour6'
 import Modal from './Pages/Modal'
+import ScrollToTop from './Components/ScrollToTop'
 
 function App() {
 
 
   const [showModal, setShowModal] = useState(false);
-  
+
 
 
   return (
     <>
+      <ScrollToTop />
       <Header />
-      
+
       <Routes>
+      
         <Route path='/' element={<Login />} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Home' element={<Home />} />
@@ -37,17 +40,17 @@ function App() {
         <Route path='*' element={<Error />} />
         <Route path='/Card' element={<Card />} />
         <Route path='/Tour1' element={<Tour1 setShowModal={setShowModal} />} />
-        <Route path='/Tour2' element={<Tour2 setShowModal={setShowModal}/>} />
-        <Route path='/Tour3' element={<Tour3 setShowModal={setShowModal}/>} />
-        <Route path='/Tour4' element={<Tour4 setShowModal={setShowModal}/>} />
-        <Route path='/Tour5' element={<Tour5 setShowModal={setShowModal}/>} />
-        <Route path='/Tour6' element={<Tour6 setShowModal={setShowModal}/>} />
-       <Route path='/Modal' element={showModal && <Modal setShowModal={setShowModal} />}/>
+        <Route path='/Tour2' element={<Tour2 setShowModal={setShowModal} />} />
+        <Route path='/Tour3' element={<Tour3 setShowModal={setShowModal} />} />
+        <Route path='/Tour4' element={<Tour4 setShowModal={setShowModal} />} />
+        <Route path='/Tour5' element={<Tour5 setShowModal={setShowModal} />} />
+        <Route path='/Tour6' element={<Tour6 setShowModal={setShowModal} />} />
+        <Route path='/Modal' element={showModal && <Modal setShowModal={setShowModal} />} />
       </Routes>
       {showModal && (
         <Modal setShowModal={setShowModal} />
       )}
-        
+
 
 
       <Footer />
